279fac54db56f0eaeb88dc27e6148f426bf019ed
Nikhil Gupta <76448370+Nikhilgupta814@users.noreply.github.com>
2022-10-11 20:17:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import requests
 
 def Fectch_poster_path(movie_id):
 
-    response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US'. format(movie_id))
+    response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US'.format(movie_id))
     data = response.json()
     return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
 
